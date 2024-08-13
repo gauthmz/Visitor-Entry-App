d5ccb9f349d94afe2e295f8c1ca5132dfc5fd70a
gauthmz <gauthambabu12@gmail.com>
2024-08-13 11:26:19 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import AddVisitor from './components/AddVisitor';
 import ViewAllVisitors from './components/ViewAllVisitors';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddVisitor/>
-      <ViewAllVisitors/>
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path='/' element={<AddVisitor/>}/>
+        <Route path='/view' element={<ViewAllVisitors/>}/>
+
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
